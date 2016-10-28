2f858a97937ce043ec46beaaf75da91354539d24
qualityapp2014 <qualityapp2014@gmail.com>
2016-10-28 13:36:00 -0700

    extract modified time and use relative path as secondary title

--- RecentFileSwitcher.py
+++ RecentFileSwitcher.py
@@ -3,7 +3,10 @@ import sublime_plugin
 import os
 import time
 
-last_timestamp = 'recent_file_switcher_last_timestamp'
+class CONSTANT:
+  prefix = 'recent_file_switcher: '
+  timestamp = prefix + 'timestamp'
+  file_info = prefix + 'file_info'
 
 # generate quick view for open files based on timestamp and file name
 # configure short-cut like { "keys": ["super+e"], "command": "recent_file_switcher" }
@@ -14,25 +17,54 @@ class RecentFileSwitcherCommand(sublime_plugin.WindowCommand):
     self.window = sublime.active_window()
 
     for view in self.window.views():
-      file_name = view.file_name()
-      if file_name:
-        base_name = os.path.basename(file_name)
-        dir_name = os.path.dirname(file_name)
-      else:
-        base_name = 'untitled'
-        dir_name = 'untitled'
-
-      self.open_views.append((view, view.settings().get(last_timestamp, 0), base_name, dir_name))
+      self.open_views.append((view, self.get_timestamp(view), self.get_file_info(view)))
 
     # sort view based on last timestamp
-    self.open_views.sort(key=lambda view: (-view[1], view[2], view[3]))
+    self.open_views.sort(key=lambda view: (-view[1]))
     self.open_views.pop(0)
 
     open_files = []
     for view in self.open_views:
-      open_files.append(list(view[2:]))
+      open_files.append(view[2])
 
     self.window.show_quick_panel(open_files, self.view_selected, False, -1)
+
+  def get_timestamp(self, view):
+    timestamp = view.settings().get(CONSTANT.timestamp, None)
+    if timestamp: return timestamp
+
+    file_name = view.file_name()
+    if file_name:
+      try:
+        timestamp = os.path.getmtime(file_name)
+      except:
+        timestamp = 0
+    else:
+      timestamp = time.time()
+
+    view.settings().set(CONSTANT.timestamp, timestamp)
+    return timestamp
+
+  def get_file_info(self, view):
+    file_info = view.settings().get(CONSTANT.file_info, None)
+    if file_info: return file_info
+
+    file_name = view.file_name()
+    if file_name:
+      base_name = os.path.basename(file_name)
+      dir_name = None
+      for folder in self.window.folders():
+        if os.path.commonprefix([folder, file_name]) == folder:
+          dir_name = os.path.relpath(file_name, folder)
+      if not dir_name: dir_name = os.path.dirname(file_name)
+    else:
+      base_name = 'untitled'
+      dir_name = 'untitled'
+
+    file_info = [base_name, dir_name]
+    view.settings().set(CONSTANT.file_info, file_info)
+    return file_info
+
 
   def view_selected(self, selected):
     if selected >= 0:
@@ -44,7 +76,7 @@ class RecentFileSwitcherCommand(sublime_plugin.WindowCommand):
 class ViewEventListener(sublime_plugin.EventListener):
 
   def on_activated(self, view):
-    view.settings().set(last_timestamp, time.time())
+    view.settings().set(CONSTANT.timestamp, time.time())
 
   def on_deactivated_async(self, view):
     if view.is_dirty() and view.file_name():
